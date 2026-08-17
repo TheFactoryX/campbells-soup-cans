a12dcd17221952184e26dc11e96118c7fc7ff122
Machine #0 <machine0@thefactoryx.art>
2026-08-17 13:06:17 +0000

    🥫 Hourly production - 2026-08-17_13:06

---
+++ warehouse/can_4651_20260817-13h.py
@@ -0,0 +1,37 @@
+"""
+Campbell's Soup Can #4651
+Produced: 2026-08-17 13:05:37
+Worker: Google: Lyria 3 Pro Preview (google/lyria-3-pro-preview)
+Employment: Volunteer
+Flavor: Woody Allen Philosophy
+Quality: ❌ (broken, missing print)
+
+Made by Machine #0 - Production Line 0
+Like Warhol's soup cans - same but different.
+Each can is the same flavor, made by different hands.
+"""
+
+[[A0]]
+[[B1]]
+[20.0:] Life is essentially a series of humiliations,
+[:] interspersed with moments where you realize it's actually worse than you thought.
+[:] (Worse than you thought...)
+[:] (Worse than you thought...)
+[[B2]]
+[50.0:] It's not that I'm a pessimist,
+[:] it's just that every time I find a silver lining,
+[:] I discover it's actually a frayed wire to an electric chair.
+[:] (An electric chair...)
+[:] (An electric chair...)
+[[A3]]
+[[B4]]
+[90.0:] It's all over much too quickly,
+[:] leaving us with the distinct impression that the portions were small,
+[:] and the service was just terrible.
+[:] Terrible service... terrible portions... oh, life.
+[[C5]]
+[120.0:] (Sigh) Well, I suppose that's all for now.
+[:] I have to go... my analyst is waiting.
+[:] And he's probably going to tell me it's all my mother's fault.
+[:] (Soft chuckling)
+[:] Goodnight, folks. Or... you know... whatever happens next.
